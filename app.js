b97935e4461aeb69ea93747abb8dfd1cea0af179
quader00 <lauraquade@gmx.de>
2023-03-25 14:12:23 +0100

    mlö

---
+++ app.js
@@ -0,0 +1,10 @@
+function hoverImage(event) {
+    var x = document.querySelectorAll("#hover_img>img");
+    var sDisplay = (event.type === "mouseout") ? "none" : "block";
+    x[0].style.display = sDisplay;
+  }
+  function hoverImage(event) {
+    var x = document.querySelectorAll("#image1>img");
+    var sDisplay = (event.type === "mouseout") ? "none" : "block";
+    x[0].style.display = sDisplay;
+  }
