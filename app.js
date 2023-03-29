1a5e798c3e70e964ab6396be6ac6c2437a16cdee
quader00 <lauraquade@gmx.de>
2023-03-29 15:10:00 +0200

    caSJO

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-function hoverImage(event) {
-    var x = document.querySelectorAll("#hover_img>img");
-    var sDisplay = (event.type === "mouseout") ? "none" : "block";
-    x[0].style.display = sDisplay;
-  }
-  function hoverImage(event) {
-    var x = document.querySelectorAll("#image1>img");
-    var sDisplay = (event.type === "mouseout") ? "none" : "block";
-    x[0].style.display = sDisplay;
-  }
+
+
+var map = L.map('map').setView([51.505, -0.09], 13);
